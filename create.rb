af485fceba0930dfe40d477c20b9619569301d25
gmalicki@gmail.com <gmalicki@gmail.com>
2009-05-30 13:03:59 -0400

    ip tracker

--- create.rb
+++ create.rb
@@ -44,7 +44,10 @@ end
 def sleep_until_browser_closed
   loop do
     sleep 2
-    break unless firefox_running? 
+    break unless firefox_running?
+    if $browser.url == "http://mail.live.com/?rru=inbox"
+      $browser.close
+    end
   end
 end
 
